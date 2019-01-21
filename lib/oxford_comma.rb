23a5762bc0a8f2f8ad93d55576d79dd30eb851b0
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-21 20:07:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
   if array.length == 1 
-    
+    array.join
+  elsif array.length == 2 
+  else 
   last = array.last
   array.pop
   "#{array.join(", ")}, and #{last}"
+end
 end
