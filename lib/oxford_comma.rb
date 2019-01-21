51b4573f5ef9e7bb2e2e253776336862144c98d0
Nicolas Bermudez <bermnicolas@gmail.com>
2019-01-21 20:08:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+  elsif array.length == 2 
+    "#{array[0]} and #{array[1]}"
+  else 
+    last = array.last
+    array.pop
+    "#{array.join(", ")}, and #{last}"
+   end
 end
